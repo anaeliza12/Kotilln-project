58a41f0af49259973e2b9a6d2837eff5809897f9
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-14 22:22:09 -0300

    First commit: MainActivity creation, activity_main.xml creation and produto_item.xml

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Binding: processo de vinculacao do código fonte com o arquivo de layout
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.setText("Cesta de frutas")
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        nome.setText("Laranja, uva e maca")
+
+        val valor = findViewById<TextView>(R.id.valor)
+        nome.setText("Laranja, uva e maca")
+
+
+
+    }
+}
+
